7d9ec56c7263025fa2c704f0ae2358d289fc932e
Om Juvatkar <omjuvatkar123@gmail.com>
2026-01-26 15:59:50 +0530

    connect DB to the server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,25 @@
-import { prisma } from './lib/prisma.js'
+import {app} from "./app.js"
+import dotenv from "dotenv"
+import { prisma } from "./lib/prisma.js"
 
-async function main() {
-  // Example: Fetch all records from a table
-  // Replace 'user' with your actual model name
-  const allUsers = await prisma.user.findMany()
-  console.log('All users:', JSON.stringify(allUsers, null, 2))
+dotenv.config({
+  path: "./.env"
+})
+
+const PORT = process.env.PORT || 8000
+
+async function startServer() {
+  try {
+    await prisma.$connect();
+    console.log("DB Connected Successfully");
+    
+    app.listen(PORT, ()=>{
+      console.log(`Server is running on port ${PORT}`);
+    })
+  } catch (err) {
+    console.log(`Failed to start server `, err);
+    process.exit(1)
+  }
 }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+startServer()
